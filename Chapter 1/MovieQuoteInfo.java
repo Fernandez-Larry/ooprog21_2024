16aa9c8da721909705d8852cd532b61921c78f61
larry-spec <laryfernandez12@gmail.com>
2024-08-14 19:35:28 +0800

    Create MovieQuoteInfo.java

---
+++ Chapter 1/MovieQuoteInfo.java
@@ -0,0 +1,8 @@
+public class LokiQuoteInfo {
+    public static void main(String[] args) {
+        System.out.println("Quote: \"I know what kind of God I need to be for you. For all of us.\"");
+        System.out.println("Show: Loki");
+        System.out.println("Character: Loki Laufeyson");
+        System.out.println("Year: 2021");
+    }
+}
