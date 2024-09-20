ffcf79d3e4b13315725ffdf306355cc6d0c0ae93
Larry Fernandez <laryfernandez12@gmail.com>
2024-09-20 19:17:24 +0800

    Create FirstAddressExercise.java

---
+++ Chapter 3/FirstAddressExercise.java
@@ -0,0 +1,14 @@
+public class FirstAddressExercise {
+
+public static void main(String[] args) {
+System.out.println("First Java Application");
+System.out.println("Oppo");
+displayAddress();
+System.out.println("Company: Oppo Inc.");
+}
+
+public static void displayAddress() {
+System.out.println("18 Haibin Road, Wusha, Chang'an");
+System.out.println("Dongguan, Guangdong, China");
+      }
+}
